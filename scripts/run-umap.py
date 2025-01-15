1a524d3ead3013dd3da3dc3b3a48f7dd8f0548ec
Chia-Chen Chu <jerry955071@gmail.com>
2025-01-15 16:04:20 +0800

    added options: n_components, target_clusters

--- scripts/run-umap.py
+++ scripts/run-umap.py
@@ -1,23 +1,53 @@
+from argparse import ArgumentParser
 import pandas as pd
 import json
 import umap
-import sys
+import os
 
-argv = sys.argv[1:]
-if len(argv) < 3:
-    print("Usage: umap.py <pca_projections.csv> <umap_projections.csv> <params.json>  <random_state>")
-    sys.exit(1)
+# create argument parser
+argParser = ArgumentParser()
+argParser.add_argument("--projections_in", help="Path to PCA projections CSV file")
+argParser.add_argument("--projections_out", help="Path to output UMAP projections CSV file")
+argParser.add_argument("--params", help="Path to UMAP parameters JSON file")
+argParser.add_argument("--random_state", help="Random state for UMAP", required=False)
+argParser.add_argument("--n_components", help="Number of components for UMAP", required=False)
+argParser.add_argument("--cell_identity", help="Cell identity file name", required=False)
+argParser.add_argument("--target_clusters", help="Target cluster for UMAP", required=False)
+
+# parse arguments
+args = argParser.parse_args()
+projections_in = args.projections_in
+projections_out = args.projections_out
+params = args.params
+random_state = args.random_state
+n_components = args.n_components
+cell_identity = args.cell_identity
+target_clusters = args.target_clusters
+
+# load data    
+projections_in = pd.read_csv(projections_in, index_col=0)
+params = json.load(open(params))
+if random_state:
+    params.update({"random_state": int(random_state)})
+if n_components:
+    params.update({"n_components": int(n_components)})
+if target_clusters:
+    if cell_identity is None:
+        raise ValueError("Cell identity file name is required to specify target cluster")
+    cell_identity = pd.read_csv(cell_identity)
+    target_clusters = set([int(i) for i in target_clusters.split(",")])
+    target_cells = cell_identity[cell_identity["Cluster"].isin(target_clusters)]["Barcode"].values
+    projections_in = projections_in.loc[target_cells]
+
     
-pca_projections = pd.read_csv(argv[0], index_col=0)
-params = json.load(open(argv[2]))
-if len(argv) == 4:
-    params.update({"random_state": int(argv[3])})
+# run UMAP
 reducer = umap.UMAP(**params)
-umap_projections = reducer.fit_transform(pca_projections)
+umap_projections = reducer.fit_transform(projections_in)
 out_df = pd.DataFrame(
     umap_projections,
-    index=pca_projections.index,
-    columns=["UMAP-1", "UMAP-2"]
+    index=projections_in.index,
+    columns=[f"UMAP-{i+1}" for i in range(int(n_components))]
 )
 out_df.index.name = "Barcode"
-out_df.to_csv(argv[1])
+os.makedirs(os.path.dirname(projections_out), exist_ok=True)
+out_df.to_csv(projections_out)
